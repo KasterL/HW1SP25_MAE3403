3e17add5e4b946781b7fa8521a0f4defe928c66e
KasterL <jamie.lam.pb@gmail.com>
2025-01-26 18:08:42 -0600

    JamieLam-HW1-MAE3403

--- stem/HW1SP23_Prob3.py
+++ stem/HW1SP23_Prob3.py
@@ -1,5 +1,6 @@
 # region imports
 import random as rnd
+import statistics as stats
 # endregion
 
 # region functions
@@ -13,9 +14,15 @@ def between(num, low, high, inclusivelow=True, inclusivehigh=True):
     :param inclusivehigh: bool to include the high end
     :return: bool indicating if between low and high limits
     """
-    # $JES MISSING CODE
-    pass
-
+    if inclusivelow and inclusivehigh:
+        return low <= num <= high
+    elif inclusivelow:
+        return low <= num <= high
+    elif inclusivehigh:
+        return low < num <= high
+    else:
+        return low < num < high
+# endregion
 
 def main():
     """
@@ -28,35 +35,43 @@ def main():
     2 & 3.  A number in bin3 will not necessarily be in 1 or 2.
     :return: nothing
     """
-    N = # $JES MISSING CODE  # size of array I want
+    N = 1000# $JES MISSING CODE  # size of array I want
     arr = []  # array for storing the numbers
-    mean = # $JES MISSING CODE  # the mean I want
-    stdev = # $JES MISSING CODE  # the standard deviation
+    mean = 0# $JES MISSING CODE  # the mean I want
+    stdev = 1# $JES MISSING CODE  # the standard deviation
 
     bin1=0  # normal dist should contain 68% within +/-1stdev
     bin2=0  # normal dist should contain 95.5% within +/-2stdev
     bin3=0  # normal dist should contain 99.7% within +/-3stdev
 
     # find edges of the limits for the various bins
-    bin1low = # $JES MISSING CODE
-    bin1high = # $JES MISSING CODE
+    bin1low = mean - stdev# $JES MISSING CODE
+    bin1high = mean + stdev# $JES MISSING CODE
 
-    bin2low = # $JES MISSING CODE
-    bin2high = # $JES MISSING CODE
+    bin2low = mean - (2*stdev)# $JES MISSING CODE
+    bin2high = mean + (2*stdev)# $JES MISSING CODE
 
-    bin3low = # $JES MISSING CODE
-    bin3high = # $JES MISSING CODE
+    bin3low = mean - (3*stdev)# $JES MISSING CODE
+    bin3high = mean + (3*stdev)# $JES MISSING CODE
 
     for i in range(N):  # this loop generates the numbers
-        arr.append(# $JES MISSING CODE)
+        num = rnd.normalvariate(mean, stdev)
+        arr.append(num)
         # this checks which bin(s) the current number is in.
-        # $JES MISSING CODE
+        if between(num, bin1low, bin1high):
+            bin1 += 1
+        if between(num, bin2low, bin2high):
+            bin2 += 1
+        if between(num, bin3low, bin3high):
+            bin3 += 1
+    # endregion
 
     #print(arr)  # If I want to see the actual array, uncomment this.
     print("{:.1f}% of data within +/-1 StDev of mean.".format(100*bin1/N))
     print("{:.1f}% of data within +/-2 StDev of mean.".format(100*bin2/N))
     print("{:.1f}% of data within +/-3 StDev of mean.".format(100*bin3/N))
-# endregion
+    # endregion
 
 if __name__ == "__main__":
     main()  # calls the function main
+# endregion
--- stem/HWSP23_Prob1.py
+++ stem/HWSP23_Prob1.py
@@ -1,8 +1,7 @@
 # region imports
-#JES MISSING CODE
 
 # endregion
-
+from Die import rollFairDie, rollUnFairDie
 # region function declarations
 def main():
     """
@@ -10,14 +9,15 @@ def main():
     it calculates and outputs the probability of each possible score.
     :return: nothing
     """
-    scores = #JES MISSING CODE  # create a list with 6 elements/items initialized to 0's
-    n = #JES MISSING CODE  # how many times to roll the die
+    scores =[0]*6  # create a list with 6 elements/items initialized to 0's
+    n = 1000  # how many times to roll the die
     for i in range(n):  # each time through the loop, roll die and increment a score
-        score = Die.rollFairDie()  # score = 1 to 6
-        scores[#JES MISSING CODE] += 1  # increment score-1 item b/c 0 indexing start
+        score = rollFairDie()  # score = 1 to 6
+        scores[score-1] += 1  # increment score-1 item b/c 0 indexing start
     # print the result
-    #JES MISSING CODE
-
+    print("After rolling fair die 1000 times")
+    for i in range(6):
+        print(f"Probability of rolling a {i+1}:{scores[i]/n:.4f}")
 
 def main2():
     """
@@ -25,7 +25,15 @@ def main2():
     it calculates and outputs the probability of each possible score.
     :return: nothing
     """
-    #JES MISSING CODE
+    scores = [0] * 6  # create a list with 6 elements/items initialized to 0's
+    n = 10000  # how many times to roll the die
+    for i in range(n):  # each time through the loop, roll die and increment a score
+        score = rollFairDie()  # score = 1 to 6
+        scores[score - 1] += 1  # increment score-1 item b/c 0 indexing start
+    # print the result
+    print("\nAfter rolling fair die 10,000 times")
+    for i in range(6):
+        print(f"Probability of rolling a {i + 1}:{scores[i] / n:.4f}")
     pass
 
 
@@ -35,7 +43,15 @@ def main3():
     it calculates and outputs the probability of each possible score.
     :return: nothing
     """
-    #JES MISSING CODE
+    scores = [0] * 6  # create a list with 6 elements/items initialized to 0's
+    n = 10000  # how many times to roll the die
+    for i in range(n):  # each time through the loop, roll die and increment a score
+        score = rollUnFairDie()  # score = 1 to 6
+        scores[score - 1] += 1  # increment score-1 item b/c 0 indexing start
+    # print the result
+    print("\nAfter rolling fair die 10,000 times")
+    for i in range(6):
+        print(f"Probability of rolling a {i + 1}:{scores[i] / n:.4f}")
     pass
 
 
